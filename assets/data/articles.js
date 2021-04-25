82adfa69f5bb190d86288342f4587489b8f8f5cb
Julien Cadart <jucadart@gmail.com>
2021-04-25 21:30:06 +0900

    templating

--- assets/data/articles.js
+++ assets/data/articles.js
@@ -1,24 +1,4 @@
-// var articles = {
-//     article_1: {
-//         title:"article1",
-//         body:"dfdfdfd",
-//         categories: [],
-//         link:"https://nbviewer.jupyter.org/github/anindya-saha/Machine-Learning-with-Python/blob/master/kaggle-bike-sharing-demand/kaggle-bike-sharing-demand.ipynb",
-//         iframeHeight:28600,
-//     }
-// }
-
-
-// // compile the template
-// function createHTML(){
-//   var rawTemplate = document.getElementById("article-template").innerHTML;
-//   var compileTemplate = Handlebars.compile(rawTemplate);
-//   var ourGeneratedHTML = compileTemplate(articles);
-
-//   var myarticle = document.getElementById("myarticle");
-//   myarticle.innerHTML = ourGeneratedHTML;
-// }
-
+//Handlebars Templating compile and data
 $(function () {
     // Grab the template script
     var theTemplateScript = $("#banner-template").html();
@@ -27,9 +7,40 @@ $(function () {
     var theTemplate = Handlebars.compile(theTemplateScript);
   
     // Define our data object
-    var articles={
-      "title": "My super Title",  
-    };
+  var articles = {
+    "articles": [
+      {
+        "id": "001",
+        "title": "My super Title",
+        "sub-title": "Kaggle Category",
+        "description": "My super Description",
+        "categories": "kaggle",
+        "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Machine-Learning-with-Python/blob/master/kaggle-bike-sharing-demand/kaggle-bike-sharing-demand.ipynb'",
+        "iframe-height":"28600",
+        "image":"images/pic10.jpg"
+      },
+      {
+        "id": "002",
+        "title": "My super Title 2",
+        "sub-title": "Spark Category",
+        "description": "My super Description 2",
+        "categories": "spark",
+        "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/working-with-nested-data-types/working-with-nested-data-types.ipynb'",
+        "iframe-height":"1100",
+        "image":"images/pic10.jpg"
+      },
+      {
+        "id": "003",
+        "title": "My super Title 3",
+        "sub-title": "Spark Category",
+        "description": "My super Description 3",
+        "categories": "spark",
+        "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
+        "iframe-height":"6100",
+        "image":"images/pic10.jpg"
+      }
+    ]
+  };
   
     // Pass our data to the template
     var theCompiledHtml = theTemplate(articles);
@@ -37,3 +48,28 @@ $(function () {
     // Add the compiled html to the page
     $('.myarticle').html(theCompiledHtml);
   });
+
+
+//Parse the article info in cookies 
+function iframeCookies(link, height) {
+  Cookies.set('link', link, { expires: 7 });
+  Cookies.set('height', height, { expires: 7 });
+}
+
+//Get article info from cookies and set it in iframe
+function iframeSetting() {
+  iSetting = document.querySelector('iframe')
+  iSetting.src = Cookies.get('link');
+  iSetting.height = Cookies.get('height');
+}
+
+// Toggle Function to show hide the menu
+function menuToggle() {
+  var elem = document.getElementById('sidebar')
+  if (elem.classList.contains("inactive")) {
+    elem.classList.remove("inactive");
+  }
+  else {
+    elem.classList.add("inactive")
+  }
+}
