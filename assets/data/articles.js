8690d6e9884f2197ba5eef0a948ecca592d285c6
Julien Cadart <jucadart@gmail.com>
2021-04-26 13:02:28 +0900

    nav menu templating

--- assets/data/articles.js
+++ assets/data/articles.js
@@ -1,11 +1,26 @@
+Handlebars.registerHelper('if_eq', function(a,b,opts){
+  if (a == b){
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+});
+
 //Handlebars Templating compile and data
 $(function () {
     // Grab the template script
     var theTemplateScript = $("#banner-template").html();
-  
+    var menuMLTemplateScript =$("#menu-template-machineLearning").html();
+    var menuDSTemplateScript =$("#menu-template-dataScience").html();
+    var menuDATemplateScript =$("#menu-template-dataAnalysis").html();
     // Compile the template
     var theTemplate = Handlebars.compile(theTemplateScript);
-  
+    // add the person partial
+    
+   var menuMLTemplate = Handlebars.compile(menuMLTemplateScript);
+   var menuDSTemplate = Handlebars.compile(menuDSTemplateScript);
+   var menuDATemplate = Handlebars.compile(menuDATemplateScript);
+
     // Define our data object
   var articles = {
     "articles": [
@@ -17,7 +32,8 @@ $(function () {
         "categories": "kaggle",
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Machine-Learning-with-Python/blob/master/kaggle-bike-sharing-demand/kaggle-bike-sharing-demand.ipynb'",
         "iframe-height":"28600",
-        "image":"images/pic10.jpg"
+        "image":"images/pic10.jpg",
+        "menu":"Machine Learning"
       },
       {
         "id": "002",
@@ -27,7 +43,8 @@ $(function () {
         "categories": "spark",
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/working-with-nested-data-types/working-with-nested-data-types.ipynb'",
         "iframe-height":"1100",
-        "image":"images/pic10.jpg"
+        "image":"images/pic10.jpg",
+        "menu":"Data Science"
       },
       {
         "id": "003",
@@ -37,16 +54,35 @@ $(function () {
         "categories": "spark",
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
         "iframe-height":"6100",
-        "image":"images/pic10.jpg"
+        "image":"images/pic10.jpg",
+        "menu":"Data Science"
+      },
+      {
+        "id": "004",
+        "title": "My super Title 4",
+        "sub-title": "Spark Category",
+        "description": "My super Description 4",
+        "categories": "spark",
+        "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
+        "iframe-height":"6100",
+        "image":"images/pic10.jpg",
+        "menu":"Data Analysis"
       }
     ]
   };
   
     // Pass our data to the template
     var theCompiledHtml = theTemplate(articles);
-  
+    var menuMLCompileHtml = menuMLTemplate(articles);
+    var menuDSCompileHtml = menuDSTemplate(articles);
+    var menuDACompileHtml = menuDATemplate(articles);
+
+ 
     // Add the compiled html to the page
-    $('.myarticle').html(theCompiledHtml);
+   $('.myarticle').html(theCompiledHtml);
+   $('.mymenu-machineLearning').html(menuMLCompileHtml);
+   $('.mymenu-dataScience').html(menuDSCompileHtml);
+   $('.mymenu-dataAnalysis').html(menuDACompileHtml);
   });
 
 
@@ -73,3 +109,5 @@ function menuToggle() {
     elem.classList.add("inactive")
   }
 }
+
+
