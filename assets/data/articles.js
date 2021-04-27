63176a5afb9263ac03edefb5ecdcded6e15e2dcb
Julien Cadart <jucadart@gmail.com>
2021-04-28 07:43:10 +0900

    spinner

--- assets/data/articles.js
+++ assets/data/articles.js
@@ -119,3 +119,16 @@ function menuToggle() {
 						header.classList.remove("sticky");
 					}
 				}
+
+
+// Query the elements
+const iframeEle = document.getElementById('iframe');
+const loadingEle = document.getElementById('loading');
+
+iframeEle.addEventListener('load', function() {
+  // Hide the loading indicator
+  loadingEle.style.display = 'none';
+
+  // Bring the iframe back
+  iframeEle.style.opacity = 1;
+});
