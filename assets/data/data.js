0fce63bd781427bf3b860fe57b034d7626cd365f
anindya-saha <anindya_elect@yahoo.com>
2021-04-28 08:34:01 -0700

    wip

--- assets/data/data.js
+++ assets/data/data.js
@@ -3,37 +3,48 @@
     "articles": [
       {
         "id": "001",
-        "title": "My super Title 1", // Used in the page body and Menu
-        "sub-title": "Kaggle Category", // Sub title used to display article category
-        "description": "My super Description", // Description of the article
-        "categories": "kaggle", // {All,kaggle,spark,machine-learning,data-science,data-analysis} 
+        "title": "Bike Sharing Demand Prediction", // Used in the page body and Menu
+        "sub-title": "kaggle,machine-learning", // Sub title used to display article category
+        "description": "Bike sharing systems are a means of renting bicycles where the process of obtaining membership, rental, and bike return is automated via a network of kiosk locations throughout a city. Using these Bike Sharing systems, people rent a bike from one location and return it to a different or same place on need basis. People can rent a bike through membership (mostly regular users) or on demand basis (mostly casual users). This process is controlled by a network of automated kiosk across the city.", // Description of the article
+        "categories": "kaggle,machine-learning", // {All,kaggle,spark,machine-learning,data-science,data-analysis} 
         //link of jupiter note book between ''
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Machine-Learning-with-Python/blob/master/kaggle-bike-sharing-demand/kaggle-bike-sharing-demand.ipynb'",
         "iframe-height":"28600", //Height of the iframe
-        "image":"images/pic10.jpg", // Picture of the article in index page
+        "image":"images/bikes.png", // Picture of the article in index page
         "menu":"Machine Learning" // Define under which menu in the side bar the article should be placed.
       },
       {
         "id": "002",
-        "title": "My super Title 2",
-        "sub-title": "Spark Category",
-        "description": "My super Description 2",
-        "categories": "spark",
+        "title": "Working with Nested Data Types",
+        "sub-title": "spark,machine-learning,data-science",
+        "description": "Recently, I needed to work with Spark dataframes having Map datatypes for one of our projects. I realized that Map and Array are the two most commonly used datatypes. So, I explored in detail how can we create, query, explode and implode columns of array and map datatypes. I created this notebook to be a handy reference for myself. Please feel free to checkout this notebook if you also need something quick and handy while working with these nested datatypes.",
+        "categories": "spark,machine-learning,data-science",
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/working-with-nested-data-types/working-with-nested-data-types.ipynb'",
-        "iframe-height":"1100",
-        "image":"images/pic10.jpg",
-        "menu":"Data Science"
+        "iframe-height":"11048",
+        "image":"images/kaggle-spotify.png",
+        "menu":"Data Science with Spark"
       },
       {
         "id": "003",
-        "title": "My super Title 3",
-        "sub-title": "Spark Category",
-        "description": "My super Description 3",
+        "title": "Advanced PySpark Dataframe and SQL",
+        "sub-title": "spark,data-science",
+        "description": "A comprehensive tutorial on Spark 2.2 DataFrame and SQL APIs and a side by side comparison of both approaches. It is based on a near real-life dataset of a fictitious Retail store provided by Cloudera. I have compiled while this I am learning and implementing Spark for Data Mining at work. The use cases implemented range from simple to quite complex to cover as many possible different Spark functions and complicated SQL variations.",
         "categories": "spark",
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
-        "iframe-height":"6100",
-        "image":"images/pic10.jpg",
-        "menu":"Data Science"
+        "iframe-height":"60359",
+        "image":"images/spark-retail-db.jpeg",
+        "menu":"Data Science with Spark"
+      },
+      {
+        "id": "004",
+        "title": "Sanfrancisco Crime Analysis",
+        "sub-title": "data-science,data-analysis",
+        "description": "In this notebook we will be analysing the Sanfrancisco crime rates dataset.",
+        "categories": "data-science,data-analysis",
+        "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
+        "iframe-height":"60359",
+        "image":"images/spark-retail-db.jpeg",
+        "menu":"Data Analysis"
       },
       {
         "id": "004",
@@ -42,7 +53,7 @@
         "description": "My super Description 4",
         "categories": "spark",
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
-        "iframe-height":"6100",
+        "iframe-height":"60359",
         "image":"images/pic10.jpg",
         "menu":"Data Analysis"
       },
@@ -53,18 +64,7 @@
         "description": "My super Description 4",
         "categories": "spark",
         "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
-        "iframe-height":"6100",
-        "image":"images/pic10.jpg",
-        "menu":"Data Analysis"
-      },
-      {
-        "id": "004",
-        "title": "My super Title 4",
-        "sub-title": "Spark Category",
-        "description": "My super Description 4",
-        "categories": "spark",
-        "jupiter-link":"'https://nbviewer.jupyter.org/github/anindya-saha/Data-Science-with-Spark/blob/master/retail-db-advanced-sql-analysis/retail-database-analysis-python.ipynb'",
-        "iframe-height":"6100",
+        "iframe-height":"60359",
         "image":"images/pic10.jpg",
         "menu":"Data Analysis"
       }
